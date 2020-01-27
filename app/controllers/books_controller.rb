c383b32092cb429a678393e6234d2ffe40226399
nakagawanatsu <nakagawa.natsu.infratop@gmail.com>
2020-01-28 00:43:35 +0900

    20200128_0043

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,16 @@ class BooksController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		
+		@book = Book.new
+		@books = Book.all
+		@user = current_user
 	end
 
 	def show
-		
+		@book = Book.new
+		@mybook = Book.find(params[:id])
+		@user = @mybook.user
+
 	end
 
 	def create
@@ -21,6 +26,22 @@ class BooksController < ApplicationController
 			@books = Book.all
 			render 'index'
 		end
+	end
+
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to books_path
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+		
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,19 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		
+		@user = current_user
+		@book = Book.new
+		@users = User.all
 	end
 
 	def show
 		@user = User.find(params[:id])
 		@book = Book.new
+		@books = @user.books.page(params[:page]).reverse_order
 	end
 
 	def edit
 		
-	end
-
-	private
-	def book_params
-		params.require(:book).permit(:title, :body)
 	end
 
 end
