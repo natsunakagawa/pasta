624793abd5dffed4a9fa2698e8479dd58f08177b
nakagawanatsu <nakagawa.natsu.infratop@gmail.com>
2020-01-27 19:02:55 +0900

    20200127_1902

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,26 @@
 class BooksController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def index
 		
 	end
 
 	def show
 		
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		@user = current_user
+		if @book.save
+			flash[:create] = "You have created book successfully"
+			redirect_to book_path(@book)
+		else
+			@books = Book.all
+			render 'index'
+		end
 	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def index
 		
 	end
 
 	def show
+		@user = User.find(params[:id])
+		@book = Book.new
+	end
+
+	def edit
 		
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 
 end
