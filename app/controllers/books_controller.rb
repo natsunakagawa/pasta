c18f225dd7e0ad1e185929444371c5e47e447522
nakagawanatsu <nakagawa.natsu.infratop@gmail.com>
2020-04-01 13:05:38 +0900

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,11 +19,11 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-		@user = current_user
 		if @book.save
 			flash[:create] = "You have created book successfully"
 			redirect_to book_path(@book)
 		else
+			@user = current_user
 			@books = Book.all
 			render 'index'
 		end
