deae2cbe349cfa28ac2631c088d316f38ae401d1
nakagawanatsu <nakagawa.natsu.infratop@gmail.com>
2020-05-06 17:04:03 +0900

    push

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_01_24_082344) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "humen", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
